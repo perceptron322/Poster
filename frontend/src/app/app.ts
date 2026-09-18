6aa7facf436df6626952aad8fe2ecd26c6bce847
Maxwell <maks.parfenov.567210@gmail.com>
2026-09-18 22:23:05 +0700

    инициализация angular проекта, подключение taiga ui

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import { TuiRoot } from '@taiga-ui/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, TuiRoot],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected readonly title = signal('frontend');
+}
