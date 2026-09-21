3948c7d898a9e66b35a03672d2ae21edd7c37937
Maxwell <maks.parfenov.567210@gmail.com>
2026-09-21 12:05:47 +0700

    исправили второй негативный сценарий

--- scripts/negative/n02_email_unique.sql
+++ scripts/negative/n02_email_unique.sql
@@ -1,3 +1,4 @@
 BEGIN;
 INSERT INTO users (name, email, role) VALUES ('Двойник', 'alice@example.com', 'customer');
+INSERT INTO users (name, email, role) VALUES ('Тройник', 'alice@example.com', 'customer');
 ROLLBACK;
